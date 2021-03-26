26f2820b7919fe2c4a84ba7c6ea19f8fc7f7e750
hadnet <hadnet1@gmail.com>
2021-03-26 14:20:44 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'react-app', // Create React App base settings
+    'eslint:recommended', // recommended ESLint rules
+    'plugin:@typescript-eslint/recommended', // recommended rules from @typescript-eslint/eslint-plugin
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
